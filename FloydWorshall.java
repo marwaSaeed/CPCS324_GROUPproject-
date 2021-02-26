8ba3892db34d21d0fb8fd2c85cd8eaf7fac1d822
marwaSaeed <52875604+marwaSaeed@users.noreply.github.com>
2021-02-26 17:48:48 +0300

    Update FloydWorshall.java

--- FloydWorshall.java
+++ FloydWorshall.java
@@ -1,13 +1,110 @@
-
+	import java.util.*;
+		import java.lang.*;
+		import java.io.*;
 public class FloydWorshall {
 
+
+	// Floyd Warshall 
+	//Group Project Phase1 
+	// Marwa , Haya, Intesar
+
+
+	    final static int inf = 99999;
+	    /////////////////////////////////////////////////////////////////////////////
+	    static void printAlpha()
+	    {
+	       char[] alpha = 
+	    		   {'A','B','C','D','E','F','G','H','I','J'};
+	       System.out.println();
+	       System.out.printf (" %-5s"," ");
+	      for(int i=0;i<10;i++) {
+	    	  
+	    	  System.out.printf (" %-5c",alpha[i]);
+	      }
+	     System.out.println();
+	     System.out.println();
+	    }
+	    //////////////////////////////////////////////////////////////////////////////
+	   static void printSolution(int dist[][])
+	    {
+		   char[] alpha = 
+    		   {'A','B','C','D','E','F','G','H','I','J'};
+    		   
+      ;
+	        for (int i=0; i<10; ++i)
+	        {
+	            System.out.printf (" %-5c",alpha[i]);
+	            for (int j=0; j<10; ++j)
+	            {
+	            	  
+	                if (dist[i][j]==inf)
+	                 
+	                System.out.printf (" %-5s",  "inf");
+	                else
+	              
+	                System.out.printf (" %-5d",  dist[i][j]);
+	            }
+	            System.out.println();
+	        }
+	    }
+	 
+	    
+	    /////////////////////////////////////////////////////////////////////////
+	   	    public static void main (String[] args)
+	    {
+	   
+	        int graph[][] = { 
+	        		 {0,10,inf, inf ,inf,5,inf,inf,inf,inf},//A
+	        		 {inf,0,3, inf ,3,inf,inf,inf,inf,inf},//B
+	        		 {inf,inf,0,4 ,inf,inf,inf,5,inf,inf},//C
+	        		 {inf,inf,inf,0,inf,inf,inf,inf,4,inf},//D
+	        		 {inf,inf,4, inf ,0,inf,2,inf,inf,inf},//E
+	        		 {inf,3,inf, inf ,inf,0,inf,inf,inf,2},//F
+	        		 {inf,inf,inf,7,inf,inf,0,inf,inf,inf},//G
+	        		 {inf,inf,inf,4,inf,inf,inf,0,3,inf},//H
+	        		 {inf,inf,inf,inf ,inf,inf,inf,inf,0,inf},//I
+	        		 {inf,6,inf,inf ,inf,inf,8,inf,inf,0},//J
+	                        };
+	     
+	        int dist[][] = new int[10][10];
+	        int i, j, k;
+	 
+	        for ( i = 0; i < 10; i++)
+	            for ( j = 0; j < 10; j++)
+	                dist[i][j] = graph[i][j];
+	 
+	        for (k = 0; k < 10; k++)
+	        {
+	           for (i = 0; i < 10; i++)
+	            {
+	                
+	                for (j = 0; j < 10; j++)
+	                {
+	                    // If vertex k is on the shortest path from i to j, then update the value of dist[i][j]
+	                
+	                    if (dist[i][k] + dist[k][j] < dist[i][j])
+	                        dist[i][j] = dist[i][k] + dist[k][j];
+	                }
+	            }
+	            System.out.println("\n\n D "+(k+1)+": \n"+"_________________________________________________________________");
+	            
+	            printAlpha();
+	            if(k==10)
+	            	System.out.println("The following matrix shows the shortest distances between every pair of vertices");
+	            printSolution(dist);
+	        }
+	 
+	     
+	       
 	
-	public static void main(String[] args) {
-		
-		
-		
-		
-	}
+	      
+	    
+	  }
+	
+	    
+	    
+	    
+	
 	
 	
 }
